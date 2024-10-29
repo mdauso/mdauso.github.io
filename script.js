2576ae09ab4b139eb1dc88e380985fc62c4cc3db
Mario Deuse <62134772+mdauso@users.noreply.github.com>
2024-10-29 09:42:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 let Temperature_Array[] ={80, 78, 77, 75, 74, 73, 71, 70, 68, 67, 65, 64, 63, 61, 60, 59, 58, 56, 55, 54, 52, 51, 50, 49, 48, 46, 45, 44, 43, 42, 41, 40, 39, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 18, 17, 16, 15, 14, 13, 12, 11, 11, 10, 9, 8, 7, 6, 5, 5, 4, 3, 2, 
                          1, 1, 0}; 
 
+/*
         document.getElementById('connectBtn').addEventListener('click', async () => {
             try {
                 // Scanne nach Geräten ohne Service UUID, stattdessen durch Namensfilterung
@@ -24,6 +25,8 @@ let Temperature_Array[] ={80, 78, 77, 75, 74, 73, 71, 70, 68, 67, 65, 64, 63, 61
             }
         });
 
+
+*/
        /* function handleSerialData(event) {
             const value = event.target.value;
             let data = new Uint8Array(value.buffer); // Umwandlung des Datenpakets in ein 8-Byte-Array
