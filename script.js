bb0156218b42eea70cc7cf744d3f9c85dc272e77
Mario Deuse <62134772+mdauso@users.noreply.github.com>
2024-10-29 08:03:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
         let targetDeviceName = 'HMSoft'; // Ersetze dies mit dem Gerätenamen deines HC-10 oder eines anderen Geräts
         let serialCharacteristic;
 
+LSU_4_9_Temperature_Array[] ={80, 78, 77, 75, 74, 73, 71, 70, 68, 67, 65, 64, 63, 61, 60, 59, 58, 56, 55, 54, 52, 51, 50, 49, 48, 46, 45, 44, 43, 42, 
+41, 40, 39, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 18, 17, 16, 15, 14, 13, 12, 11, 11, 10, 9, 8, 7, 6, 5, 5, 4, 3, 2, 
+1, 1, 0}; 
+
         document.getElementById('connectBtn').addEventListener('click', async () => {
             try {
                 // Scanne nach Geräten ohne Service UUID, stattdessen durch Namensfilterung
@@ -45,12 +49,29 @@ function handleSerialData(event) {
             const value = event.target.value;
             let data = new Uint8Array(value.buffer);
 
+
+        
             if (data.length === 8) {
+
+                    
+                
                 // Lambda-Wert: byte 0 geteilt durch 147
                 let lambdawert = data[0];
 
+                                    
+
                 // Temperaturwert: byte 1 direkt als °C
                 let sondentemperatur = data[1];
+
+                if (sondentemperatur<113) 
+                { 
+                sondentemperatur=113; 
+                } 
+                if (sondentemperatur>187) 
+                { 
+                 sondentemperatur=187; 
+                } 
+                sondentemperatur =LSU_4_9_Temperature_Array[sondentemperatur-113]+740;
 
                 updateValues(lambdawert, sondentemperatur);
             } else {
