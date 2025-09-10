728ddc210ffa1e3b0039146a6c06d07b74db8075
Mario Deuse <62134772+mdauso@users.noreply.github.com>
2025-09-10 14:50:28 +0200

    Default View angepasst

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let currentSurface = null;
 
 // 🎯 Standardkamera (Default-Ansicht)
 const DEFAULT_CAMERA = {
-  eye:    { x: 1.5, y: 1.5, z: 1.2 },
+  eye:    { x: 1.3, y: 1.3, z: 1.8 },
   center: { x: 0,   y: 0,   z: 0   },
   up:     { x: 0,   y: 0,   z: 1   }
 };
